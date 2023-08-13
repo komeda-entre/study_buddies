23f584aba21726ec95cfbbe3a5f60c1b9d2a2b30
komeda-entre <komeda.entre@gmail.com>
2023-08-13 20:56:51 +0900

    Create simple auth

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      # ログイン機能のルーティング
+      mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+        registrations: 'api/v1/auth/registrations'
+      }
+      # ログインユーザー取得のルーティング
+      namespace :auth do
+        resources :sessions, only: %i[index]
+      end
+    end
+  end
 end
---
+++ frontend/app/src/components/pages/SignIn.tsx
@@ -0,0 +1,75 @@
+import Cookies from "js-cookie";
+import { useContext, useState, FormEvent } from "react";
+import { Link, useHistory } from "react-router-dom";
+import { signIn } from "lib/api/auth";
+import { AuthContext } from "App";
+
+const SignIn: React.FC = () => {
+    const { setIsSignedIn, setCurrentUser } = useContext(AuthContext);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const history = useHistory();
+
+    const generateParams = () => {
+        const signInParams = {
+            email: email,
+            password: password,
+        };
+        return signInParams;
+    };
+
+    const handleSignInSubmit = async (e: FormEvent) => {
+        e.preventDefault();
+        const params = generateParams();
+
+        try {
+            const res = await signIn(params);
+            if (res.status === 200) {
+                Cookies.set("_access_token", res.headers["access-token"]);
+                Cookies.set("_client", res.headers["client"]);
+                Cookies.set("_uid", res.headers["uid"]);
+
+                setIsSignedIn(true);
+                setCurrentUser(res.data.data);
+
+                history.push("/");
+            }
+        } catch (e) {
+            console.log(e);
+        }
+    };
+    return (
+        <>
+            <p>サインインページです</p>
+            <form>
+                <div>
+                    <label htmlFor="email">メールアドレス</label>
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password">パスワード</label>
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </div>
+                <button type="submit" onClick={(e) => handleSignInSubmit(e)}>
+                    Submit
+                </button>
+            </form>
+            <Link to="/signup">サインアップへ</Link>
+        </>
+    );
+};
+
+export default SignIn
